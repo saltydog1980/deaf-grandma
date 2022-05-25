f2955fc23fb186a45ecbaff572157726f317a5ce
saltydog1980 <mikeelambert99@gmail.com>
2022-05-25 17:57:43 -0400

    final python exercise

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,18 @@
 def deaf_grandma():
-
-
-
+    bye_count = 0
+    prompt = input("HEY KID!")
+    while bye_count != 2:
+        if prompt == "":
+            prompt = input("WHAT?!")
+        elif prompt == "GOODBYE!":
+            if bye_count == 0:
+                prompt = input("LEAVING SO SOON?")
+                bye_count += 1
+            elif bye_count == 1:
+                bye_count += 1
+                print("LATER, SKATER!")
+        elif prompt.isupper():
+            prompt = input("NO, NOT SINCE 1946!")
+        else:
+            prompt = input("SPEAK UP, KID!")
 deaf_grandma()
